b0296c22e4ee43ccc136e1d137dfa3e0b7e3a298
Liam Guymon <liamnguy@login02.rc.byu.edu>
2026-03-16 13:51:08 -0600

    In human readable

--- src/Solve.cpp
+++ src/Solve.cpp
@@ -9,20 +9,20 @@ int main()
 	string line;
 	string dArray[16];
 	ifstream params ("params.dat");
+
 	if (params.is_open())
 	{
-		params.seekg (16);
-		while (getline(params,line))
-		{
-			for (int i=0; i < 16; ++i)
+		params.seekg(16);
+
+		for (int i=0; i < 16; ++i)
 			{
 				params >> dArray[i];
 			}
-		}
-		int size_Array = sizeof(dArray);
-		for (int i=0, i < size_Array, i++)
+
+		for (int i=0; i < 16; i++)
 		{
 			cout << dArray[i] << " ";
 		}
+	params.close();
 	}
 }
