b92a37a0fbd07ca9e9228d960d97974f48743b1e
Liam Guymon <liamnguy@login02.rc.byu.edu>
2026-03-16 15:06:06 -0600

    Working on solving the differential

--- src/Solve.cpp
+++ src/Solve.cpp
@@ -6,21 +6,39 @@ using namespace std;
 
 int main()
 {
-	double dArray[16];                     //Initialize array
+	double dArray[16];          //Initialize array
 	ifstream params ("params.dat");
 
 	if (params.is_open())
 	{
 		string textline;
-		getline(params, textline);    //Skip first line
+		getline(params, textline);          //Skip first line
 
 		for (int i=0; i < 16; ++i)
 			{
-				params >> dArray[i];  //Insert whitespace separated values into corresponding elements of the array
+				params >> dArray[i];          //Insert whitespace separated values into corresponding elements of the array
 			}
 
-		params.close();               //Close read file
+		params.close();          //Close read file
 	}
 
-	
+	double delta_t[8];
+	double num_t[8];
+	for (int i=0; i<16; i += 2)          //Separate array into two vectors for delta t, and number of steps of t
+	{
+		for (int j=0; j<8; ++j)
+		{
+			delta_t[j] = dArray[i];
+		}
+	}
+	for (int i=1; i<16; i += 2)
+	{
+		for (int j=0; j<8; ++j)
+		{
+			num_t[j] = dArray[i];
+		}
+	}
+
+	double x_i = 1
+	double x
 }
