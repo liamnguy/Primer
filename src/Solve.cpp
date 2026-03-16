84cbb1f97aa7233fbb00d60dd53128e367c6a90a
Liam Guymon <liamnguy@login03.rc.byu.edu>
2026-03-16 11:46:24 -0600

    Reading from params.dat

--- src/Solve.cpp
+++ src/Solve.cpp
@@ -6,4 +6,19 @@ using namespace std;
 
 main int()
 {
-//Git check	
+	string line;
+	string dArray[16];
+	ifstream params ("params.dat");
+	if (params.is_open())
+	{
+		params.seekg (0,16);
+		while (getline(params,line))
+		{
+			for (int i=0; i < 16; ++i)
+			{
+				params >> dArray[i];
+			}
+		}
+		cout << dArray
+	}
+}
