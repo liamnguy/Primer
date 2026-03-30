b0a3228e27ca691444768654ec1308cdde4b39a5
Liam Guymon <liamnguy@login02.rc.byu.edu>
2026-03-30 16:48:17 -0600

    New refined basic outline. Overflow output though

--- src/Solve.cpp
+++ src/Solve.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 int main()
 {
-	double dArray[16];          //Initialize array
+	vector<double> delta_t(7,0);
+	vector<double> num_t(7,0);
+
 	ifstream params ("params.dat");
 
 	if (params.is_open())
@@ -15,36 +17,14 @@ int main()
 		string textline;
 		getline(params, textline);          //Skip first line
 
-		for (int i=0; i < 16; ++i)
-			{
-				params >> dArray[i];          //Insert whitespace separated values into corresponding elements of the array
-			}
+		for (int i=0; i < 8; i++)
+		{
+			params >> delta_t[i];
+			params >> num_t[i];
+		}
 
 		params.close();          //Close read file
 	}
-
-	vector<double> delta_t(8,0);
-	vector<double> num_t(8,0);
-	for (int i=0; i<16; i += 2)          //Separate array into two vectors for delta t, and number of steps of t
-	{
-		for (int j=0; j<8; ++j)
-		{
-			delta_t.insert(delta_t.begin()+j, dArray[i]);
-		}
-	}
-	for (int i=1; i<16; i += 2)
-	{
-		for (int j=0; j<8; ++j)
-		{
-			num_t.insert(num_t.begin()+j, dArray[i]);
-		}
-	}
 	for (int i=0; i<delta_t.size(); i++)
-	{
-		cout << delta_t.at(i);
-	}
-	for (int i=0; i<num_t.size(); i++)
-	{
-		cout << num_t.at(i);
-	}
+		cout << delta_t.at(i) << ", " << num_t.at(i) << "\n";
 }
