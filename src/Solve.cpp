ccbffba8b21aae7e6240829434be33eaa84bdcaf
Liam Guymon <liamnguy@login02.rc.byu.edu>
2026-04-06 16:09:45 -0600

    FINALLY! Vectors WORK!!

--- src/Solve.cpp
+++ src/Solve.cpp
@@ -5,26 +5,35 @@
 #include <vector>
 using namespace std;
 
+vector<double> Solve (vector<double> a, vector<double> b)
+{
+	
+
 int main()
 {
-	vector<double> delta_t(7,0);
-	vector<double> num_t(7,0);
+	vector<double> delta_t;
+	vector<double> num_t;
 
 	ifstream params ("params.dat");
 
 	if (params.is_open())
 	{
-		string textline;
-		getline(params, textline);          //Skip first line
-
-		for (int i=0; i < 8; i++)
+		string line;
+		getline(params, line);
+		while (getline(params, line))
 		{
-			params >> delta_t[i];
-			params >> num_t[i];
-		}
-
-		params.close();          //Close read file
+			istringstream iss(line);
+			double a, b;
+			if (iss >> a >> b)
+			{
+				delta_t.push_back(a);
+				num_t.push_back(b);
+			}
+		}	
+		params.close();
 	}
-	for (int i=0; i<delta_t.size(); i++)
-		cout << delta_t.at(i) << ", " << num_t.at(i) << "\n";
+	for (int i=0; i<delta_t.size(); ++i)
+	{
+		cout << delta_t.at(i) << ", " << num_t.at(i) << endl;
+	}
 }
